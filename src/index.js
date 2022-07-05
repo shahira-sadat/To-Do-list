00489a39d95b265f7cf8461789456e084e763084
shahira-sadat <shahira.sadat1@gmail.com>
2022-07-05 21:43:45 +0430

    Add todo list and create list function

--- src/index.js
+++ src/index.js
@@ -24,3 +24,21 @@ let todoList = [
 ];
 
 localStorage.setItem('todo', JSON.stringify(todoList));
+
+
+const todoContainer = document.querySelector('#todo');
+const createList = () => {
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    todoList.forEach((item) => {
+        const li = document.createElement('li');
+        li.innerHTML 
+        = `<button class="check-item">
+            <i class="fa-regular fa-square">
+            </i>${item.description}</button>
+            <button class="move-item">
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+            </button>`;
+        todoContainer.insertBefore(li, todoContainer.children[item.index]);
+    });
+};
+createList();

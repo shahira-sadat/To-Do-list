aab6aeb768e809533e1c261b5bd32186ade761d6
shahira-sadat <shahira.sadat1@gmail.com>
2022-07-05 21:38:17 +0430

    Create todoList array and include data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./style.css"
+
+let todoList = [
+    {
+        description: 'Reading',
+        isCompleted: false,
+        index: 3,
+    },
+    {
+        description: 'Listening',
+        isCompleted: false,
+        index: 2,
+    },
+    {
+        description: 'Writing',
+        isCompleted: false,
+        index: 1,
+    },
+    {
+        description: 'Speaking',
+        isCompleted: false,
+        index: 0,
+    },
+];

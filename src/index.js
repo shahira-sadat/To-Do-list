1c5d82fd78b2388fb4642550b5d9cfdcccc0676e
shahira-sadat <shahira.sadat1@gmail.com>
2022-07-06 06:13:06 +0430

    Create object of tasks from class Tasks

--- src/index.js
+++ src/index.js
@@ -1,43 +1,5 @@
 import './style.css';
 import Tasks from './tasks.js';
 
-let todoList = [
-  {
-    description: 'Reading',
-    isCompleted: false,
-    index: 3,
-  },
-  {
-    description: 'Listening',
-    isCompleted: false,
-    index: 2,
-  },
-  {
-    description: 'Writing',
-    isCompleted: false,
-    index: 1,
-  },
-  {
-    description: 'Speaking',
-    isCompleted: false,
-    index: 0,
-  },
-];
+const tasks = new Tasks();
 
-localStorage.setItem('todo', JSON.stringify(todoList));
-
-const todoContainer = document.querySelector('#todo');
-const populateList = () => {
-  todoList = JSON.parse(localStorage.getItem('todo'));
-  todoList.forEach((item) => {
-    const li = document.createElement('li');
-    li.innerHTML = `<button class="check-item">
-            <i class="fa-regular fa-square">
-            </i>${item.description}</button>
-            <button class="move-item">
-            <i class="fa-solid fa-ellipsis-vertical"></i>
-            </button>`;
-    todoContainer.insertBefore(li, todoContainer.children[item.index]);
-  });
-};
-populateList();

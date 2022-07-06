e52e1034bc2a45284810cd0a86257e7e9bb68345
shahira-sadat <shahira.sadat1@gmail.com>
2022-07-06 06:17:04 +0430

    Add code for add-new-task and refresh the todo list

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import Tasks from './tasks.js';
 
 const tasks = new Tasks();
 
+tasks.populateList();
+
+
+const newInput = document.querySelector('#new-task');
+newInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && newInput.value) {
+    tasks.add(newInput.value);
+    newInput.value = '';
+  }
+});
+
+
+const refreshBtn = document.querySelector('#refresh-list');
+refreshBtn.addEventListener('click', () => {
+  document.location.reload();
+});
